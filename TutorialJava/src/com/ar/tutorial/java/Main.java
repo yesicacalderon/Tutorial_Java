67acf72529e6eb0fd76a608aae7ef3d4adf1a69e
Yesica Calderon <yeecalderon21@gmail.com>
2020-02-24 21:35:58 -0300

    Se crea primera clase de prueba

---
+++ TutorialJava/src/com/ar/tutorial/java/Main.java
@@ -0,0 +1,14 @@
+package com.ar.tutorial.java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Persona persona1= new Persona ("yesica","calderon",18);
+		
+		System.out.println(persona1.getApellido());
+		
+	}
+
+}
